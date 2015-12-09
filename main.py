de3c97fa57246ac1861dc93e0524db3680dafaaa
harry48225 <harrybest@live.co.uk>
2015-12-09 21:49:30 +0000

    Sort of working needs some tweaks

---
+++ main.py
@@ -0,0 +1,37 @@
+'''
+Created on 9 Dec 2015
+
+@author: Harry
+'''
+
+import random
+
+
+words = ['Hello', 'Goodbye']
+
+n = random.randint(0,1)
+
+chosenWord = list(words[n])
+lettersfound = []
+print chosenWord
+
+for letter in chosenWord:
+    
+    lettersfound.append('_')
+
+while True:
+    print lettersfound    
+    letter = raw_input('Please enter a letter: ')
+
+    
+    if letter in chosenWord:
+        
+        while True:
+            if letter in chosenWord:
+                x = chosenWord.index(letter)
+                lettersfound[x] = chosenWord[x]
+                chosenWord[x] = '_'
+            else:
+                break
+        print 'That was correct!'
+        print chosenWord
