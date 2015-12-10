98890d6c7fc0aba5a249d9e24435a03f03bfc00e
harry48225 <harrybest@live.co.uk>
2015-12-10 18:15:22 +0000

    Added comments and a play again system

--- main.py
+++ main.py
@@ -9,64 +9,76 @@ Created on 9 Dec 2015
 @author: Harry
 '''
 
-import random
+import random #Needed for random words
 
 
-words = ['HELLO', 'GOODBYE']
-
-n = random.randint(0,1)
-
-chosenWordMaster = list(words[n])
-chosenWord = list(words[n])
-lettersfound = []
-lettersGuessed = []
-lives = 8
+words = ['HELLO', 'GOODBYE'] # The list of all of the words
 
 
-for letter in chosenWord:
+while True: #The main loop
     
-    lettersfound.append('_')
+    n = random.randint(0,1)             #
+                                        #
+    chosenWordMaster = list(words[n])   #
+    chosenWord = list(words[n])         # These set up lists and choose the word to be guessed
+    lettersfound = []                   #
+    lettersGuessed = []                 #
+    lives = 8                           #
     
-
-
-while lives > 0:
     
-    print 'Letters guessed: ',
-    for letter in lettersGuessed:
-        print letter + '',
-        
-    print  
-    print 'Lives remaining: %d ' % (lives)
-    for letter in lettersfound:
-        print letter,
-   
-   
-    print      
-    letter = raw_input('Please enter a letter: ').upper()
-    while letter in lettersGuessed:
-        print 'You have already guessed that letter!'
-        letter = raw_input('Please enter a letter: ').upper()
-        
-
-    if letter in chosenWord:
-        
-        while True:
-            if letter in chosenWord:
-                x = chosenWord.index(letter)
-                lettersfound[x] = chosenWord[x]
-                chosenWord[x] = '_'
-            else:
-                break
-        print 'That was correct!'
-        
-    else:
-        print 'That was wrong!'
-        lives -= 1
-    lettersGuessed.append(letter)
-    if lettersfound == chosenWordMaster:
-        
-        print 'You win!'
-        break
+    for letter in chosenWord:           #
+                                        # Adding underscores to the hidden list to show the user how many letters
+        lettersfound.append('_')        #
+                                
     
-if lives == 0:
-    print 'Gameover'
+    
+    while lives > 0:                    # This runs until the user guesses wrong 8 times
+        
+        print 'Letters guessed: ',      #
+        for letter in lettersGuessed:   # Showing the user the letters that have already been guessed
+            print letter + '',          # 
+            
+        print  
+        print 'Lives remaining: %d ' % (lives)  #
+        for letter in lettersfound:             # This prints the sections of the word that they have already found
+            print letter,                       #
+       
+       
+        print      
+        letter = raw_input('Please enter a letter: ').upper()       #
+        while letter in lettersGuessed:                             # Getting them to guess a letter and making sure that it
+            print 'You have already guessed that letter!'           # is one that they haven't already guessed
+            letter = raw_input('Please enter a letter: ').upper()   #
+            
+    
+        if letter in chosenWord:        # Checking if the letter that they have guesses is in the word
+            
+            while True:                                 #
+                if letter in chosenWord:                #
+                    x = chosenWord.index(letter)        #
+                    lettersfound[x] = chosenWord[x]     # Adding the letters that they have guessed to the list and replacing the under scores in it.
+                    chosenWord[x] = '_'                 #
+                else:                                   #
+                    break                               #
+            print 'That was correct!'                   #
+            
+        else:
+            print 'That was wrong!'     # Letter is not in the chosen word
+            lives -= 1
+        lettersGuessed.append(letter)
+        
+        if lettersfound == chosenWordMaster: # If they have found the entire word
+            
+            print 'You win!'
+            break
+        
+    if lives == 0:  # Printing game over if they run out of lives
+        print 'Gameover'
+        
+    again = raw_input('Would you like to play again? y/n: ')    #
+    while again.upper() not in ['YES', 'Y', 'NO', 'N']:         # Want to play again?
+        print 'Error invalid response!'                         #
+        again = raw_input('Would you like to play again? y/n: ')#
+    
+    if again in ['NO', 'NO']:
+        break                   # Breaking out of the entire loop and therefore ending the program if they do not want to play again
